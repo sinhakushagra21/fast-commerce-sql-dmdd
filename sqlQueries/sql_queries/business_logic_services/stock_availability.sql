010fbc92f41dfc4accd0e7da2e3fe3a1740f43c2
Akash-Malhotra121 <malhotra.aka@northeastern.edu>
2024-11-19 13:15:28 -0500

    Inventory order validation (#7)
    * Stock Availabity business rule implemented
    
    * invntory order validation
    
    ---------
    
    Co-authored-by: Akash Malhotra <akashmalhotra@Akashs-MacBook-Air.local>

---
+++ sqlQueries/sql_queries/business_logic_services/stock_availability.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION check_stock_availability (
+    p_product_id IN NUMBER,
+    p_required_quantity IN NUMBER
+) RETURN VARCHAR2 IS
+    v_current_stock NUMBER;
+    v_threshold NUMBER := 5;
+BEGIN
+    SELECT item_quantity
+    INTO v_current_stock
+    FROM user18gp.products
+    WHERE product_id = p_product_id;
+
+    IF v_current_stock = 0 THEN
+        RETURN 'Out of Stock';
+    ELSIF v_current_stock < p_required_quantity THEN
+        RETURN 'Insufficient stock to fulfill the requested quantity';
+    ELSIF v_current_stock <= v_threshold THEN
+        RETURN 'Low Stock';
+    ELSE
+        RETURN 'In Stock';
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 'Product not found';
+    WHEN OTHERS THEN
+        RETURN 'An error occurred while checking stock availability';
+END;
+/
+
+
+SET SERVEROUTPUT ON;
+DECLARE
+    v_status VARCHAR2(50);
+BEGIN
+    v_status := check_stock_availability(101, 3);
+    DBMS_OUTPUT.PUT_LINE(v_status); 
+END;
+/
