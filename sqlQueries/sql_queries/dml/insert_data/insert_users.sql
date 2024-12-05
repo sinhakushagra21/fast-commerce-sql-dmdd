c1aa9445ac99fd44868f742ecd9bec763ca27b1b
Akash-Malhotra121 <malhotra.aka@northeastern.edu>
2024-12-04 22:12:43 -0500

    added data in tables (#16)
    Co-authored-by: Akash Malhotra <akashmalhotra@Akashs-MacBook-Air.local>

--- sqlQueries/sql_queries/dml/insert_data/insert_users.sql
+++ sqlQueries/sql_queries/dml/insert_data/insert_users.sql
@@ -6,5 +6,19 @@ BEGIN
         WHEN DUP_VAL_ON_INDEX THEN
             NULL; -- Ignore duplicate
     END;
+
+    BEGIN
+        INSERT INTO USERS (USER_ID, IS_ACTIVE, CONTACT_ID) VALUES (2, 'Y', 2);
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            NULL; -- Ignore duplicate
+    END;
+
+    BEGIN
+        INSERT INTO USERS (USER_ID, IS_ACTIVE, CONTACT_ID) VALUES (3, 'Y', 2);
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            NULL; -- Ignore duplicate
+    END;
 END;
 /
